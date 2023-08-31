74c2163e3b33c872b70461e1bdfcdbf2d3417f6a
kwonmoto <kwonhyukjun@lguplus.co.kr>
2023-08-31 13:03:36 +0900

    test: useQuery example code

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import useGetTestInfo from "../api/test/useGetTestInfo";
 
 export default function Home() {
+  // const { data } = useGetTestInfo();
+
+  // console.log(data);
+  
   return (
     <div>
       <h1>Home</h1>
