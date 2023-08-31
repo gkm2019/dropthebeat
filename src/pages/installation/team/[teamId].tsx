2650df64249a79977cdbb71e1c7f651a51245be7
Kwon HyukJun <95995470+kwonmoto@users.noreply.github.com>
2023-08-31 17:43:15 +0900

    chore: nextjs로 변경 (#4)
    * chore: pacakges 설치 및 axios setting (#2)
    
    * test: axios test
    
    * test: useQuery example code
    
    * feat: Install Page UI 구현
    
    * chore: nextjs로 변경 (#4)

--- src/pages/installation/team/[teamId].tsx
+++ src/pages/installation/team/[teamId].tsx
@@ -1,9 +1,5 @@
-import { useRouter } from "next/router";
 import TeamInstallScreen from "../../../screen/TeamInstallScreen";
 
 export default function TeamInstallPage() {
-  const router = useRouter();
-  const { teamId } = router.query as { teamId: string };
-
   return <TeamInstallScreen />;
 }
