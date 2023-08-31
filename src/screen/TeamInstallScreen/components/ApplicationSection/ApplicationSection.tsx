4feeb99d33573d5872f16b0709680c570feea3e2
Kwon HyukJun <95995470+kwonmoto@users.noreply.github.com>
2023-08-31 17:35:00 +0900

    chore: nextjs로 변경 (#4)

--- src/screen/TeamInstallScreen/components/ApplicationSection/ApplicationSection.tsx
+++ src/screen/TeamInstallScreen/components/ApplicationSection/ApplicationSection.tsx
@@ -3,20 +3,8 @@ import * as S from "./ApplicationSection.styled";
 import ApplicationCard from "../ApplicationCard/ApplicationCard";
 
 export default function ApplicationSection() {
-  const [platform, setPlatform] = useState("");
-
-  useEffect(() => {
-    const userAgent = window.navigator.userAgent.includes("Mac")
-      ? "Mac"
-      : "Windows";
-
-    setPlatform(userAgent);
-  }, []);
-
   return (
     <S.Container>
-      <S.Title>Applications & packages to install</S.Title>
-      <S.Title>Your OS is {platform}</S.Title>
       <S.CardContainer>
         <ApplicationCard />
         <ApplicationCard />
